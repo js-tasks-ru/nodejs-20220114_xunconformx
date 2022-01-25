03730143852a0e3f7c1a310e2125b67d86bf0805
k.dubov <example@example.com>
2022-01-25 22:39:36 +0300

    Add module 03-streams/04-file-server-post

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -12,6 +14,61 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      const stream = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitedStream = new LimitSizeStream({limit: 1000000});
+
+      stream.on('error', (err) => {
+        if (pathname.includes('/')) {
+          res.statusCode = 400;
+          res.end("400");
+
+          return;
+        }
+
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File already exists.');
+
+          return;
+        }
+
+        res.statusCode = 500;
+        res.end('Server error.');
+      });
+
+      stream.on('close', () => {
+        res.statusCode = 201;
+        res.end('Saved!');
+      });
+
+      limitedStream.on('error', (error) => {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end('File must be 1Mb at max.');
+        } else {
+          res.statusCode = 500;
+          res.end('Server error.');
+        }
+
+        stream.destroy();
+
+        fs.unlink(filepath, (err) => {
+          if (err) throw err;
+          console.log(`successfully deleted ${filepath}`);
+        });
+      });
+
+      req.on("aborted", () => {
+        limitedStream.destroy();
+        stream.destroy();
+
+        fs.unlink(filepath, (err) => {
+          if (err) throw err;
+          console.log(`successfully deleted ${filepath}`);
+        });
+      });
+
+      req.pipe(limitedStream).pipe(stream);
 
       break;
 
