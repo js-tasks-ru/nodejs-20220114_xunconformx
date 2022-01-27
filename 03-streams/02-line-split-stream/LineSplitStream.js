ba2de71d92acbc25452ae028685ff60cbf457a49
k.dubov <example@example.com>
2022-01-27 21:14:54 +0300

    Add module 03-streams/02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,32 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  lines = '';
+
   constructor(options) {
     super(options);
+
+    this.encoding = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
+    this.lines += chunk.toString(this.encoding);
+    
+    const linesArr = this.lines.split(os.EOL);
+    const lastLineIndex = linesArr.length - 1;
+
+    linesArr.forEach((line, index) => {
+      index === lastLineIndex ? this.lines = line : this.push(line);
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.lines);
+    this.lines = '';
+
+    callback();
   }
 }
 
