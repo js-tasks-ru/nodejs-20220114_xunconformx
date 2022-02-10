1fcd5a0a4e0d6fbf609b72212581d804fdf2d16c
k.dubov <example@example.com>
2022-02-10 15:55:42 +0300

    Fix module testing-configuration-logg\unit-tests

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -18,111 +18,38 @@ describe('testing-configuration-logging/unit-tests', () => {
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
       expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
     });
-    it('валидатор проверяет строковые поля (макс)', () => {
-      const validator = new Validator({
-        name: {
-          type: 'string',
-          min: 10,
-          max: 20,
-        },
-      });
 
-      const errors = validator.validate({ name: 'developmenttechsdbsdbdsbsdbsdb' });
-
-      expect(errors).to.have.length(1);
-      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
-      expect(errors[0]).to.have.property('error').and.to.be.equal('too long, expect 20, got 30');
-    });
-    it('валидатор проверяет строковые поля (нет ошибок)', () => {
-      const validator = new Validator({
-        name: {
-          type: 'string',
-          min: 10,
-          max: 20,
-        },
-      });
-
-      const errors = validator.validate({ name: 'development' });
-
-      expect(errors).to.have.length(0);
-    });
-    it('валидатор проверяет числовые поля', () => {
+    it('валидатор проверяет числовые поля на минимум', () => {
       const validator = new Validator({
         age: {
           type: 'number',
           min: 18,
-          max: 60,
+          max: 33,
         },
       });
 
-      const errors = validator.validate({ age: 70 });
-
-      expect(errors).to.have.length(1);
-      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
-      expect(errors[0]).to.have.property('error').and.to.be.equal('too big, expect 60, got 70');
-    });
-    it('валидатор проверяет числовые поля (мин)', () => {
-      const validator = new Validator({
-        age: {
-          type: 'number',
-          min: 18,
-          max: 60,
-        },
-      });
-
-      const errors = validator.validate({ age: 17 });
+      const errors = validator.validate({age: 17});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('age');
       expect(errors[0]).to.have.property('error').and.to.be.equal('too little, expect 18, got 17');
     });
-    it('валидатор проверяет числовые поля (нет ошибок)', () => {
+
+    it('валидатор проверяет числовые поля на максимум', () => {
       const validator = new Validator({
         age: {
           type: 'number',
           min: 18,
-          max: 60,
+          max: 33,
         },
       });
 
-      const errors = validator.validate({ age: 18 });
-
-      expect(errors).to.have.length(0);
-    });
-    it('валидатор проверяет тип', () => {
-      const validator = new Validator({
-        surname: {
-          type: 'string',
-          min: 4,
-          max: 200,
-        },
-      });
-
-      const value = {surname: []};
-
-      const errors = validator.validate(value);
+      const errors = validator.validate({age: 34});
 
       expect(errors).to.have.length(1);
-
-      expect(errors[0]).to.have.property('field').and.to.be.equal('surname');
-      expect(errors[0]).to.have.property('error').and.to.be.equal(
-          `expect ${validator.rules.surname.type}, got ${typeof value.surname}`,
-      );
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too big, expect 33, got 34');
     });
-    it('валидатор проверяет тип (нет ошибок)', () => {
-      const validator = new Validator({
-        surname: {
-          type: 'string',
-          min: 4,
-          max: 200,
-        },
-      });
 
-      const value = {surname: 'Ivanov'};
-
-      const errors = validator.validate(value);
-
-      expect(errors).to.have.length(0);
-    });
   });
 });
