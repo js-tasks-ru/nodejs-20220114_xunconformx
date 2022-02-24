99a9538c72b66bc0a7e76d68fad09f6c93bc0060
k.dubov <example@example.com>
2022-02-24 21:48:18 +0300

    Add module 05-koajs/01-chat-app

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,31 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    await new Promise((resolve) => {
+        subscribers.push(resolve);
+    }).then((message) => {
+        ctx.status = 200;
+        ctx.response.body = message;
+        next();
+    });
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message;
+    ctx.status = 200;
+
+    if (! message) {
+        return next();
+    }
+    subscribers.forEach((resolve) => {
+        resolve(message);
+    });
+    subscribers = [];
+    ctx.response.body = 'published';
+    next();
 });
 
 app.use(router.routes());
